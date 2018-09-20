5f1d3f79844786ff479bb81d05eae1e4086e394d
mauricci <33112636+MaurizioRicci@users.noreply.github.com>
2018-09-20 10:47:57 +0200

    Rewrite for KickAss plugin. They made some changes in their site. Now it's not possible anymore to download a file (they use JS to insert magnet link in page). Seed and Leech are also removed by search page

--- kickass_torrent.py
+++ kickass_torrent.py
@@ -1,9 +1,10 @@
-#VERSION: 1.1
+#VERSION: 1.2
 #AUTHORS: mauricci
 
 from helpers import retrieve_url
 from helpers import download_file, retrieve_url
 from novaprinter import prettyPrinter
+import re
 
 try:
     #python3
@@ -15,59 +16,69 @@ except ImportError:
 class kickass_torrent(object):
     url = 'https://kickass-cr.online'
     name = 'KickAss torrent'
-    supported_categories = {'all': 'all', 'movies': 'movies', 'tv': 'tv', 'music': 'music', 'games': 'games'}
+    supported_categories = {'all': 'all'}
     
     class MyHTMLParser(HTMLParser):
 
+        def __init__(self):
+            HTMLParser.__init__(self)
+            self.url = 'https://kickass-cr.online'
+            self.insideDataTd = False
+            self.tableFound = False
+            self.tdCount = -1
+            self.tableCount = -1
+            self.infoMap = {'name':0,'size':1}
+            self.fullResData = []
+            self.singleResData = self.getSingleData()
+            
         def getSingleData(self):
-            return {'name':'-1','seeds':'-1','leech':'-1','size':'-1','link':'-1','desc_link':'-1','engine_url':'KickAss',}
-
-        insideTd = False
-        tableFound = False
-        tdCount = -1
-        infoMap = [('name',0),('size',1),('seeds',4),('leech',5)]
-        fullResData = []
-        singleResData = getSingleData(None)
+            return {'name':'-1','seeds':'-1','leech':'-1','size':'-1','link':'-1','desc_link':'-1','engine_url':self.url}
         
         def handle_starttag(self, tag, attrs):
             #print("Encountered a start tag:", tag)
             if tag == 'table':
                 self.tableFound = True
-            if self.tableFound and tag == 'td':
+                self.tableCount += 1
+            if tag == 'td':
                 self.tdCount += 1
-                self.insideTd = True
+                if self.tableCount == 1:
+                    self.insideDataTd = True
             if self.tableFound and tag == 'a' and len(attrs) > 0:
                  Dict = dict(attrs)
-                 if Dict.get('class','').find('cellMainLink') != -1 and 'href' in Dict:
-                     self.singleResData['desc_link'] = 'https://kickass-cr.online' + Dict['href']
-                 if 'href' in Dict and Dict['href'].find('magnet') != -1:
-                     self.singleResData['link'] = Dict['href']
+                 if self.infoMap['name'] == self.tdCount and 'href' in Dict:
+                     self.singleResData['desc_link'] = self.url + '/' + Dict['href']
+                     self.singleResData['link'] = self.singleResData['desc_link']
 
         def handle_endtag(self, tag):
             if tag == 'table':
                 self.tableFound = False
             if tag == 'td':
-                self.insideTd = False
+                self.insideDataTd = False
             if tag == 'tr':
                 self.tdCount = -1
                 if len(self.singleResData) > 0:
                     #ignore trash stuff
-                    if self.singleResData['name'] != '-1' and self.singleResData['size'].find(',') == -1 \
+                    if self.singleResData['name'] != '-1' and self.isValidSize(self.singleResData['size']) \
                       and not self.singleResData['name'].startswith('Advertising'):
+                        print(self.singleResData)
                         prettyPrinter(self.singleResData)
                         self.fullResData.append(self.singleResData)
                     self.singleResData = self.getSingleData()
 
         def handle_data(self, data):
-            if self.insideTd:
-                for Tuple in self.infoMap:
-                    if self.tdCount == Tuple[1]:
-                        currKey = Tuple[0]
-                        if data.strip() != '':
+            if self.insideDataTd:
+                for key,val in self.infoMap.items():
+                    if self.tdCount == val:
+                        currKey = key
+                        if currKey in self.singleResData and data.strip() != '':
                             if self.singleResData[currKey] == '-1':
                                 self.singleResData[currKey] = data.strip()
                             else:
                                 self.singleResData[currKey] += data.strip()
+
+        #true if the size is valid (must contain number and no ',')
+        def isValidSize(self, size):
+            return size.find(',') == -1 and bool(re.search(r'\d', size))
 
         #remove who uploaded the torrent
         def adjustName(self):
@@ -83,12 +94,13 @@ class kickass_torrent(object):
     # DO NOT CHANGE the name and parameters of this function
     # This function will be the one called by nova2.py
     def search(self, what, cat='all'):
+        what = what.replace('%20','-')
         currCat = self.supported_categories.get(cat,'all')
         parser = self.MyHTMLParser()
 
         #analyze firt 10 pages of results
         for currPage in range(1,11):
-            url = self.url+'/usearch/{0}%20category:{1}/{2}/?field=seeders&sorder=desc'.format(what,currCat,currPage)
+            url = self.url+'/search/{0}/{1}/'.format(what,currPage)
             html = retrieve_url(url)
             parser.feed(html)
             parser.adjustName()
@@ -98,8 +110,8 @@ class kickass_torrent(object):
 
 
     def download_torrent(self, info):
-            """ Downloader """
-            print(download_file(info))
+        """ Downloader """
+        #KickAss html does not contain tor or magnet link. They are added with javascript
 
 if __name__ == "__main__":
     k = kickass_torrent()
