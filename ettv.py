2983aadc2b8967dcce7dec905a2aac8f4fb428d6
mauricci <33112636+MaurizioRicci@users.noreply.github.com>
2018-09-25 22:16:31 +0200

    Fixed bug that caused seeders count over 999 to display as uknown

--- ettv.py
+++ ettv.py
@@ -1,4 +1,4 @@
-#VERSION: 1.0
+#VERSION: 1.1
 #AUTHORS: mauricci
 
 from helpers import retrieve_url
@@ -57,6 +57,7 @@ class ettv(object):
                     #ignore trash stuff
                     if self.singleResData['name'] != '-1' \
                         and self.isValidSize(self.singleResData['size']):
+                        self.fixSeedOrLeechNumber(self.singleResData)
                         prettyPrinter(self.singleResData)
                         self.fullResData.append(self.singleResData)
                     self.singleResData = self.getSingleData()
@@ -78,9 +79,16 @@ class ettv(object):
             self.tdCount = -1
             self.tableCount = -1
             
-        #true if the size is valid (must contain number and no ',')
+        #true if the size is valid (mustn't contain 'n/a' or commas)
         def isValidSize(self, size):
             return not bool(re.search(r'(?:n/a)|(?:\,)', str(size).lower()))
+        
+        #seed and leech number greater than 999 are separeted by comma
+        #prettyPrinter wants them without comma, so we remove it
+        def fixSeedOrLeechNumber(self,Dict):
+            for key in ['seeds','leech']:
+                if Dict[key].count(',') == 1:
+                    Dict[key] = Dict[key].replace(',','')
 
     # DO NOT CHANGE the name and parameters of this function
     # This function will be the one called by nova2.py
